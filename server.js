5d136eb0b3e45ba89e31b296f5361efa5c744498
Erandi <roshanie101@gmail.com>
2022-05-23 23:13:50 +0530

    Create group student backend added

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+const adminRoutes = require("./routes/admin");
+const researchGroupRoutes = require("./routes/ResearchGroupRoutes");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(adminRoutes);
+app.use(researchGroupRoutes);
 
-const PORT =8000;
-
-
+const PORT = 8000;
 
 const DB_URL = process.env.DB_URI;
 
-
-mongoose.connect(DB_URL,{
+mongoose
+  .connect(DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology:  true
-})
-.then(()=>{
-    console.log('DB is connected');
-})
-.catch((err)=>console.log('DB connection err',err));
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((err) => console.log("DB connection err", err));
 
-
-app.listen(PORT, ()=>{
-    console.log(`App is running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
 });
-
